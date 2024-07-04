f94e83ac2f2077f941f1244cbbcb23302f01c6f3
kainy01 <kainykawai@gmail.com>
2024-07-04 12:30:34 +0300

    connected db

---
+++ config/dbConnection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log("Connected to database", connect.Connection.host, connect.Connection.name);
+  } catch (error) {
+    console.log("Error connecting to database", error);
+    process.exit(1);
+  }
+};
+module.exports= connectDb;
+
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 
 const dotenv = require("dotenv").config();
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.json()); // parse data stream from the client 
+app.use(express.json()); // parse data stream from the client
 app.use("/api/contacts", require("./routes/contactsRoutes"));
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  
 });
