6d71f6598edf87ede9f0100b140e1e8d49e03739
kainy01 <kainykawai@gmail.com>
2024-07-07 15:03:22 +0300

    Feat: installed and set jwt and token

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,34 +1,74 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 //@desc Register new user
 //@route Post /api/user
 //@access Public
 const reigsterUser = asyncHandler(async (req, res) => {
-  const { name, email, phone } = req.body;
-  if (!name || !email || !phone) {
-    return res.status(400).json({ message: "Please enter all fields" });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
   }
-
-  const users = await User.create({
-    name,
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  //Hshing the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
     email,
-    phone,
+    username,
+    password: hashedPassword,
   });
-  res.status(201).json(users);
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
+  res.status(201).json(user);
 });
+
 //@desc Login user
 //@route Post /api/user/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).json({ message: "Please enter all fields" });
+    res.status(400);
+    throw new Error("Please enter all fields");
+  }
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          id: user._id,
+          email: user.email,
+          username: user.username,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    return res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credentials");
   }
 });
 
 //@desc current user
 //@route Post /api/user/current
-//@access Public
+//@access Private
 const currentUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -38,5 +78,5 @@ const currentUser = asyncHandler(async (req, res) => {
 module.exports = {
   reigsterUser,
   loginUser,
-  currentUser
+  currentUser,
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-  name: {
+  username:{
     type: String,
-    required: [true, "Name is required"],
+    required: [true, "Username is required"],
   },
+ 
   email: {
     type: String,
     required: [true, "Email is required"],
+    unique: true,
   },
-  phone: {
+ 
+  password: {
     type: String,
-    required: [true, "Phone is required"],
+    required: [true, "Password is required"],
   },
   date: { type: Date, default: Date.now },
 });
